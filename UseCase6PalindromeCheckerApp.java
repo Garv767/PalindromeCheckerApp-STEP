4fe9eafb3de1c513771fb900ebd34f72761ed055
Garv Rahut <85278023+Garv767@users.noreply.github.com>
2026-02-24 10:02:00 +0000

    feat: implemented a hardcoded string palindrome checker using Queue &  Stack

---
+++ UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * MAIN CLASS - UseCase6PalindromeCheckerApp
+ * 
+ * Use Case 6: Queue + Stack Fairness Check
+ * 
+ * Description:
+ * This class demonstrates palindrome validation using
+ * two different data structures:
+ * 
+ * - Queue (FIFO - First In First Out)
+ * - Stack (LIFO - Last In First Out)
+ * 
+ * Characters are inserted into both structures and then
+ * compared by removing from the front of the queue and
+ * the top of the stack.
+ * 
+ * If all characters match, the input string is confirmed
+ * as a palindrome.
+ * 
+ * This use case helps understand how FIFO and LIFO 
+ * behaviors can be combined for symmetric comparison.
+ * 
+ * @author Developer
+ * @version 6.0
+ */
+public class UseCase6PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC6.
+     * 
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+
+        // Create a Stack to store characters in LIFO order
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);  // Enqueue (FIFO)
+            stack.push(c); // Push (LIFO)
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            // dequeue() gets the first character, pop() gets the last
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
