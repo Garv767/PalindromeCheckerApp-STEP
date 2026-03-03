812b0dae5ea3056f163aa05d39589e7b2150746b
Garv Rahut <85278023+Garv767@users.noreply.github.com>
2026-03-03 10:39:33 +0000

    feat: implement Object-Oriented Palindrome Service

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -26,7 +26,7 @@ class PalindromeChecker {
 }
 
 
-public class UseCase11PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
